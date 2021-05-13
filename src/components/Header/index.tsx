3e2ce63a9082148d81dc0ce6b0d3964424f288ef
Savio Barbosa <saviiobarbosa@gmail.com>
2021-05-12 21:07:03 -0300

    Finalização da terceira aula do NLW5 React

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,24 @@
+import format from 'date-fns/format';
+import ptBR from 'date-fns/locale/pt-BR';
+
+import Link from 'next/link';
+
+import styles from './styles.module.scss';
+
+export function Header() {
+    const currentDate = format(new Date(), 'EEEEEE, d MMM', {
+        locale: ptBR
+    });
+
+    return (
+        <header className={styles.headerContainer}>
+            <Link href={"/"}>
+                <img src="/logo.svg" alt="Podcastr" style={{ cursor: 'pointer' }} />
+            </Link>
+
+            <p>O melhor para você ouvir, sempre</p>
+
+            <span>{currentDate}</span>
+        </header>
+    );
+}
